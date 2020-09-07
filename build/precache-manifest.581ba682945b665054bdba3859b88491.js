fddea0fd156e5eec718a2240e64d3caba07ba895
Jordi Ros <shine.3p@gmail.com>
2020-09-07 18:07:17 +0200

    Prebuilt site to avoid to npm install and build with react if you just want the app

---
+++ build/precache-manifest.581ba682945b665054bdba3859b88491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753c89365e46b921fd9e23a0e561f642",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59b26b491ff584418ca5",
+    "url": "/static/css/main.07a26a2d.chunk.css"
+  },
+  {
+    "revision": "5bec456c6178de8775ad",
+    "url": "/static/js/2.b1673680.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b1673680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b26b491ff584418ca5",
+    "url": "/static/js/main.5cdf637d.chunk.js"
+  },
+  {
+    "revision": "9f9de6aadf631f936bcc",
+    "url": "/static/js/runtime-main.1535f9a9.js"
+  }
+]);
